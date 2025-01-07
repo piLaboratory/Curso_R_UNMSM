e26f61970a9e99f892763d8f845a4fd4ecde2def
piklprado <prado@ib.usp.br>
2025-01-07 04:27:54 -0300

    Cambia el tutorial funciones apply

--- Funciones_apply/initLesson.R
+++ Funciones_apply/initLesson.R
@@ -1,4 +1,21 @@
-# Code placed in this file fill be executed every time the
-# lesson is started. Any variables created here will show up in
-# the user's working directory and thus be accessible to them
-# throughout the lesson.
+# Path to installed lesson
+.lessonpath <- file.path(path.package("swirl"), "Courses", 
+                         "Programando_en_R", "lapply_and_sapply")
+# Path to dataset
+.datapath <- file.path(.lessonpath, "flag.data.txt")
+# Load dataset
+flags <- read.csv(.datapath, header=FALSE)
+# Set column names
+colnames(flags) <- c("name", "landmass", "zone", "area", "population",
+                     "language", "religion", "bars", "stripes", "colours",
+                     "red", "green", "blue", "gold", "white", "black",
+                     "orange", "mainhue", "circles", "crosses", "saltires",
+                     "quarters", "sunstars", "crescent", "triangle",
+                     "icon", "animate", "text", "topleft", "botright")
+# Path to dataset info
+.infopath <- file.path(.lessonpath, "flag.names.txt")
+# Function for user to open info
+viewinfo <- function() {
+  file.edit(.infopath)
+  return(.infopath)
+}
